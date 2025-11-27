17972ef02585d635e227f2eab8e9f2b58edb6594
ahmetshbz1 <ahmetshbz1@outlook.com>
2025-11-27 23:26:01 +0300

    fix: use homebrew-tap releases for DMG hosting

--- Casks/synapse.rb
+++ Casks/synapse.rb
@@ -2,10 +2,10 @@ cask "synapse" do
   version "1.0.0"
   sha256 "85263c4a1d2453079acc50dcd498031a4c9bc35b9aaa069359dfa3d4d530b244"
 
-  url "https://github.com/ahmetshbz1/Synapse/releases/download/v#{version}/Synapse.dmg"
+  url "https://github.com/ahmetshbz1/homebrew-tap/releases/download/v#{version}/Synapse.dmg"
   name "Synapse"
   desc "Modern clipboard manager for macOS"
-  homepage "https://github.com/ahmetshbz1/Synapse"
+  homepage "https://github.com/ahmetshbz1/homebrew-tap"
 
   depends_on macos: ">= :sonoma"
 
