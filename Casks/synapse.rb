159a3d88f32fb8fd20d56469fb8bdd104e54583d
ahmetshbz1 <ahmetshbz1@outlook.com>
2025-11-28 16:58:49 +0300

    chore: Synapse 1.1.0'a güncellendi
    - Option+Space ile popover açma özelliği
    - Klavye kısayolları iyileştirildi

--- Casks/synapse.rb
+++ Casks/synapse.rb
@@ -1,6 +1,6 @@
 cask "synapse" do
-  version "1.0.0"
-  sha256 "85263c4a1d2453079acc50dcd498031a4c9bc35b9aaa069359dfa3d4d530b244"
+  version "1.1.0"
+  sha256 "4c64469604d67b17199e261a591bd978c988bd9194bc9f934c5ec42eeec9a2ea"
 
   url "https://github.com/ahmetshbz1/homebrew-tap/releases/download/v#{version}/Synapse.dmg"
   name "Synapse"
