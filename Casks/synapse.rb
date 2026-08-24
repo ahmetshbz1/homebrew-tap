3982356b7e565c99859311874b9fc88900d9053d
Ahmetshbzz <ahmetcanshbz@proton.me>
2026-08-24 23:54:28 +0200

    release: v2.6.0

--- Casks/synapse.rb
+++ Casks/synapse.rb
@@ -1,6 +1,6 @@
 cask "synapse" do
-  version "2.5.0"
-  sha256 "eda28a70b50ad78e697333f9f893ee340fbb92f561e006e20da63508997d47ef"
+  version "2.6.0"
+  sha256 "aca834e2f0e0eddd7d39103314d44d9242b7736a0e4934c764fa8555f7acd81d"
 
   url "https://github.com/ahmetshbz1/homebrew-tap/releases/download/v#{version}/Synapse.zip"
   name "Synapse"
